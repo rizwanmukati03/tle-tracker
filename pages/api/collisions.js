b501c0d52a628e27e034de7a1afab2781d3d9fd8
Rizwan Mukati <40403812+rizwanmukati03@users.noreply.github.com>
2026-07-02 23:27:33 +0500

    Increase FORCE_COOLDOWN_SECONDS to 1 hour

--- pages/api/collisions.js
+++ pages/api/collisions.js
@@ -17,7 +17,7 @@ const SATELLITES = [
 ];
 
 const CACHE_TTL_SECONDS = 6 * 60 * 60;
-const FORCE_COOLDOWN_SECONDS = 10 * 60;
+const FORCE_COOLDOWN_SECONDS = 60 * 60;
 
 const TCA_PATTERN = /^\d{4}-\d{2}-\d{2}\s+\d{2}:\d{2}:\d{2}/;
 const NORAD_PATTERN = /^\d{4,6}$/;
